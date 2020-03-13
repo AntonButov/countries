d0f738103967f6c42dfc09f4c45aec25bb12ee90
Anton Butov <butov6101@gmail.com>
2020-03-13 14:14:47 +0300

    First cummit.

---
+++ app/src/main/java/pro/butovanton/countries/Activity2.java
@@ -0,0 +1,14 @@
+package pro.butovanton.countries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
---
+++ app/src/main/java/pro/butovanton/countries/Countrie.java
@@ -0,0 +1,8 @@
+package pro.butovanton.countries;
+
+public class Countrie {
+    String name;
+    String capital;
+    String currency;
+    String flag;
+}
